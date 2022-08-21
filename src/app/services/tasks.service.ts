0dca1ddff580bf5b8389bc67f773e1aa6a0ceaa9
Wellington Oliveira <owellington.nascimento@gmail.com>
2022-08-21 20:35:43 -0300

    Fix bug

--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -67,7 +67,7 @@ export class TasksService implements OnInit {
   }
 
   deleteTask(id: Task['id']) {
-    this.tasks.splice(this.tasks.findIndex(task => task.id === id));
+    this.tasks.splice(this.tasks.findIndex(task => task.id === id), 0);
     this.saveTasks();
   }
 
