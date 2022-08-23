8e7a15487b7d674a6ea2e6ebc2b545f126ecebe8
Wellington Oliveira <owellington.nascimento@gmail.com>
2022-08-23 12:43:57 -0300

    Add animation to list; change delete modal size

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,10 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { Task, TasksService } from '../services/tasks.service';
 
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+  query,
+} from '@angular/animations';
+
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
-  styleUrls: ['./task-list.component.scss']
+  styleUrls: ['./task-list.component.scss'],
+  animations: [
+    trigger('shrinkOut', [
+      state('in', style({ height: '*', opacity: '1', padding: '*' })),
+      state('void', style({ height: 0, opacity: '0', paddingTop: '0', paddingBottom: '0' })),
+      transition('in <=> void', [
+        animate('150ms linear')
+      ])
+    ]),
+    trigger('shrinkOutMessage', [
+      state('in', style({ display: 'block' })),
+      state('void', style({ display: 'none' })),
+      transition('void => in', [
+        //animate('150ms 400ms ease-in-out'),
+        animate('0ms 150ms'),
+      ]),
+      transition('in => void', [
+        animate(0),
+      ]),
+    ]),
+  ]
 })
 export class TaskListComponent implements OnInit {
 
